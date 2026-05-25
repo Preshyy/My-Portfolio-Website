4f7a1639fda6fb9d35c2ca2d2d05bc197a775197
PARZIVAL <53568169+Preshyy@users.noreply.github.com>
2026-05-25 19:55:38 +0100

    Added a new project to the Projects component, including the image, title, demo and repository links.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactTailwind from '../assets/projects/react-tailwind.png'
 import HTMLCSS from '../assets/projects/html-css.png'
 import FigmaImg from '../assets/figma.png'
+import ReactFirebase from '../assets/projects/react-firebase.png'
 
 const Projects = () => {
   return (
@@ -62,6 +63,26 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
+
+                {/* Grid Item 4 */}
+                <div style={{backgroundImage: `url(${ReactFirebase})`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center hover:scale-110 duration-500 py-4 items-center mx-auto content-div'>
+                    {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-xl font-bold text-white tracking-wider'>Firebase Authenticator</span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://web-authenticator-ruddy.vercel.app/" target="_blank" rel="noopener noreferrer">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="https://github.com/Preshyy/Web-Authenticator" target="_blank" rel="noopener noreferrer">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Repository</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Grid Item 5 */}
+
             </div>
         </div>
     </div>
